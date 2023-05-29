875718ef91e151d1975dee5bae7d831f6dc85d29
LuisCesarArruda <87197539+LuisCesarArruda@users.noreply.github.com>
2023-05-29 16:52:50 -0300

    termino do CRUD

--- Back-end/src/routes/memories.ts
+++ Back-end/src/routes/memories.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
+import {z} from "zod";
 
 export async function memoriesRoute(app:FastifyInstance) {
 	app.get("/memories", async () => {
@@ -17,19 +18,89 @@ export async function memoriesRoute(app:FastifyInstance) {
 		});
 	});
 
-	app.get("/memories/:id", async () => {
-		
+	app.get("/memories/:id", async (request) => {
+
+		const paramScheme = z.object(
+			{
+				id: z.string().uuid(),
+
+			}
+		);
+		const {id} = paramScheme.parse(request.params);
+		const memory = await prisma.memory.findUniqueOrThrow({
+			where: {
+				id,
+			}
+		});
+		return memory;
 	});
-    
-	app.post("/memories", async () => {
-		
+	
+	app.post("/memories", async (request) => {
+		const bodyScheme = z.object(
+			{
+				content: z.string(),
+				coverUrl: z.string(),
+				isPublic: z.coerce.boolean().default(false),
+
+			}
+		);
+		const{content, coverUrl, isPublic } = bodyScheme.parse(request.body);
+		const memory = await prisma.memory.create({
+			data: {
+				content,
+				coverUrl,
+				isPublic,
+				userId: "8600857f-aaf8-4573-8f51-4ccb1ed9113b"
+			}
+		});
 	});
 
-	app.put("/memories/:id", async () => {
-		
+	app.put("/memories/:id", async (request) => {
+		const paramScheme = z.object(
+			{
+				id: z.string().uuid(),
+
+			}
+		);
+		const {id} = paramScheme.parse(request.params);
+
+		const bodyScheme = z.object(
+			{
+				content: z.string(),
+				coverUrl: z.string(),
+				isPublic: z.coerce.boolean().default(false),
+				
+			}
+		);
+		const{content, coverUrl, isPublic } = bodyScheme.parse(request.body);
+
+		const memory = await prisma.memory.update({
+			where: {
+				id,
+			},
+			data: {
+				content,
+				coverUrl,
+				isPublic
+			}
+		});
+		return memory;
 	});
     
-	app.delete("/memories/:id", async () => {
+	app.delete("/memories/:id", async (request) => {
 		
+		const paramScheme = z.object(
+			{
+				id: z.string().uuid(),
+
+			}
+		);
+		const {id} = paramScheme.parse(request.params);
+		await prisma.memory.delete({
+			where: {
+				id,
+			}
+		});
 	});
+	
 }
